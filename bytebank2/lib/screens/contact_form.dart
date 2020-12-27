ccac5b4bc063c5cd48bbc5baf803872a314d47dc
Higor <higorpiresoliveira@gmail.com>
2020-12-27 16:55:05 -0300

    :sparkles: Add new project - course 2

---
+++ bytebank2/lib/screens/contact_form.dart
@@ -0,0 +1,71 @@
+import 'package:bytebank2/database/dao/contact_dao.dart';
+import 'package:flutter/material.dart';
+import 'package:bytebank2/models/contact.dart';
+
+class ContactForm extends StatefulWidget {
+  @override
+  _ContactFormState createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _accountNumberController =
+      TextEditingController();
+  final ContactDao _contactDao = ContactDao();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New contact'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Full name',
+              ),
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8),
+              child: TextField(
+                controller: _accountNumberController,
+                decoration: InputDecoration(
+                  labelText: 'Account number',
+                ),
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: SizedBox(
+                width: double.maxFinite,
+                child: RaisedButton(
+                  child: Text('Create'),
+                  onPressed: () => _createContact(context),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _createContact(BuildContext context) {
+    final String name = _nameController.text;
+    final int accountNumber = int.tryParse(_accountNumberController.text);
+
+    final Contact newContact = Contact(0, name, accountNumber);
+    _contactDao.save(newContact).then((id) => Navigator.pop(context));
+  }
+}
---
+++ bytebank2/lib/screens/contacts_list.dart
@@ -0,0 +1,96 @@
+import 'package:bytebank2/database/dao/contact_dao.dart';
+import 'package:bytebank2/models/contact.dart';
+import 'package:flutter/material.dart';
+
+import 'contact_form.dart';
+
+class ContactsList extends StatefulWidget {
+  @override
+  _ContactsListState createState() => _ContactsListState();
+}
+
+class _ContactsListState extends State<ContactsList> {
+  final ContactDao _contactDao = ContactDao();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: FutureBuilder<List<Contact>>(
+        initialData: List(),
+        future: _contactDao.findAll(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    SizedBox(height: 10),
+                    Text('Carregando...'),
+                  ],
+                ),
+              );
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contact> contacts = snapshot.data;
+
+              return ListView.builder(
+                itemCount: contacts.length,
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts[index];
+                  return _ContactItem(contact);
+                },
+              );
+
+              break;
+          }
+
+          return Text('Unknown error');
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => ContactForm()))
+              .then((value) => setState(() {}));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  const _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
